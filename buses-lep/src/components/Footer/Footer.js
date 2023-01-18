2e82e02877f2350d904526b2540d31f09c777d61
Dario Viada <dario.viada@bitlogic.io>
2023-01-18 17:03:31 -0300

    fix footer error and null data home

--- buses-lep/src/components/Footer/Footer.js
+++ buses-lep/src/components/Footer/Footer.js
@@ -16,14 +16,21 @@ const Footer = () => {
         <div className="container py-4 d-flex flex-wrap">
           {data.linkBlock.length !== 0 &&
             data.linkBlock.map((block) => (
-              <FooterLinkBlock key={block._key} links={block.links} title={block.title} />
+              <FooterLinkBlock
+                key={block._key}
+                links={block.links}
+                title={block.title}
+              />
             ))}
 
-          {data.socialMediaBlock !== null && (
-            <div className="col-12 col-md-4 ">
-              <div className="contactBlock">
-                {data.qrCode && (
-                <a href={data.qrCode.url} title={`${data.qrCode.image.alt}`} className="py-2">
+          <div className="col-12 col-md-4 flex-md-grow-1 ">
+            <div className="contactBlock">
+              {data.qrCode && (
+                <a
+                  href={data.qrCode.url}
+                  title={`${data.qrCode.image.alt}`}
+                  className="py-2"
+                >
                   <SanityImage
                     {...data.qrCode.image}
                     alt={`${data.qrCode.image.alt}`}
@@ -31,14 +38,14 @@ const Footer = () => {
                   />
                 </a>
               )}
-              <SocialMediaBlock
-                links={data.socialMediaBlock.links}
-                title={data.socialMediaBlock.title}
-              />
-              </div>
-              
+              {data.socialMediaBlock && (
+                <SocialMediaBlock
+                  links={data.socialMediaBlock.links}
+                  title={data.socialMediaBlock.title}
+                />
+              )}
             </div>
-          )}
+          </div>
         </div>
         <div className="footer__down">
           <div className="container d-flex justify-content-end align-items-center py-4">
