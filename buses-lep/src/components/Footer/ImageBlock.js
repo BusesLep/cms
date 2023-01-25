62d6d69bf273d7cccf367a5367fdc9caec8dcc95
Yuliya Sharapa <yuliya.sharapa@bitlogic.io>
2023-01-25 10:40:29 -0300

    create textBlock

--- buses-lep/src/components/Footer/ImageBlock.js
+++ buses-lep/src/components/Footer/ImageBlock.js
@@ -17,7 +17,7 @@ const ImageBlock = ({ images, title }) => {
   ));
   return (
     <div className="py-2 mt-3 contactBlock__images">
-      <h5 className="title-small mb-3">{title}</h5>
+      <h5 className="title-small mb-3 py-0">{title}</h5>
       <div className="row">{imageList}</div>
     </div>
   );
