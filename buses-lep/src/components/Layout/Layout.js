5b65ebd8c5f04b805c65da37dbd7433611e2bf55
erimasini2 <121623405+erimasini2@users.noreply.github.com>
2023-02-13 15:39:18 -0300

    add whatsapp button

--- buses-lep/src/components/Layout/Layout.js
+++ buses-lep/src/components/Layout/Layout.js
@@ -1,7 +1,8 @@
 
 import React from "react";
 import PropTypes from 'prop-types'
-import { Header, Footer} from "..";
+import { Header, Footer } from "..";
+import WhatsappLink from "../WhatsappLink/WhatsappLink";
 import ThemeProvider from "../../context/themeContext";
 
 import "./Layout.scss";
@@ -9,10 +10,12 @@ import "./Layout.scss";
 const Layout = ({ location, children }) => {
 
   return (
-    <ThemeProvider>
+    <ThemeProvider>      
       <Header location={location}/>
       <main>{children}</main>
+      <WhatsappLink />
       <Footer/>
+      
     </ThemeProvider>
   );
 };
